bbc8a2e33234b358098015dff00824f9c3b2a2fa
eka <send.raden@gmail.com>
2025-07-18 11:13:44 +0800

    point2 pertanyaan

--- OneOne_June18/points_jul18.py
+++ OneOne_June18/points_jul18.py
@@ -1,6 +1,21 @@
 '''
-1. Lowker pasca kursus dapat dari mana:
-   - koneksi koh edwin
-   - lamar sendiri : platform apa
+1. Modul yg dibilang kak Kyra, itu bisa diakses di mana?
+
+2. Dapat kerja setelah kursus
+a. potensi kendala:
+   - belum punya portfolio (paling dummy)
+   - di CV, pengalaman front end belum ada
+   - profil Github nothing special
+b. Dapat dari mana, apakah dari koneksi AHA demy / koh Edwin
+   Atau nyari sendiri, dengan resiko high competition & don't accept applicant outside US    
+   - high competition on remote job portal:
+     1. https://www.remotejobs.io/: mostly for US citizen
+     2. https://remote.com/
+     3. https://weworkremotely.com/listings/subscript-software-engineer-frontend-backend-or-full-stack-2
+    
+   - sempat dibahas test utk kerja dan sekilas Leetcode:
+     biasanya test kerja itu di test skill front end atau skill Leetcode.
+     kalau Leetcode, berarti kan harus dari sekarang latihan-nya.
+
 
 '''
